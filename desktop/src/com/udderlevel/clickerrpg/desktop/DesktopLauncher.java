7da9130d96281112161436914ba9de2fa1bddeb5
Edwin Avalos <eavalos0@gmail.com>
2016-02-25 20:31:23 -0700

    First commit

---
+++ desktop/src/com/udderlevel/clickerrpg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.udderlevel.clickerrpg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.udderlevel.clickerrpg.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
