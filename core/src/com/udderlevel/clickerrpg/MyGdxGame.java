7cc0e84d2598c209c078d0ca6a67076a34f33446
Edwin Avalos <eavalos0@gmail.com>
2016-02-26 00:01:01 -0700

    Add a player class and displays his stats

--- core/src/com/udderlevel/clickerrpg/MyGdxGame.java
+++ core/src/com/udderlevel/clickerrpg/MyGdxGame.java
@@ -4,16 +4,28 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
+	BitmapFont font;
 	Texture img;
+	Player player;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+
+
+		//Check for save data
+
+			//Create new player based off of saved data
+
+		//else Create a blank player
+		player = new Player();
 	}
 
 	@Override
@@ -21,7 +33,16 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
+		font.draw(batch, "Level: " + player.getLevel(), 0, 450);
+		font.draw(batch, "Health: " + player.getHealth(), 0, 400);
+		font.draw(batch, "Attack: " + player.getAttack(), 0, 350);
+		font.draw(batch, "Defense: " + player.getDefense(), 0, 300);
+		font.draw(batch, "Speed: " + player.getSpeed(), 0, 250);
+		font.draw(batch, "Xp: " + player.getXpCurrent(), 0, 200);
+		font.draw(batch, "Next Level: " + player.getXpNeeded(), 0, 150);
+
+
 		batch.end();
 	}
 }
---
+++ core/src/com/udderlevel/clickerrpg/Player.java
@@ -0,0 +1,86 @@
+package com.udderlevel.clickerrpg;
+
+/**
+ * An object of this class stores all the information of the player
+ * Created by Edwin on 2/25/2016.
+ */
+public class Player
+{
+    //Player states
+    public enum State
+    {
+        STATE_MOVING, STATE_FIGHTING, STATE_WON, STATE_DEAD;
+    }
+
+    //Current state of player
+    private State state;
+
+    int level;
+    int health;
+    int attack;
+    int defense;
+    int speed;
+    int xpNeeded;
+    int xpCurrent;
+
+    //Constructors
+    public Player()
+    {
+        level = 1;
+        health = 20;
+        attack = 5;
+        attack = 5;
+        defense = 5;
+        speed = 5;
+        xpNeeded = 10;
+        xpCurrent = 0;
+        state = State.STATE_MOVING;
+    }
+
+    //Getters
+    public State getState()
+    {
+        return state;
+    }
+
+    public int getLevel()
+    {
+        return level;
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+
+    public int getAttack()
+    {
+        return attack;
+    }
+
+    public int getDefense()
+    {
+        return defense;
+    }
+
+    public int getSpeed()
+    {
+        return speed;
+    }
+
+    public int getXpNeeded()
+    {
+        return xpNeeded;
+    }
+
+    public int getXpCurrent()
+    {
+        return xpCurrent;
+    }
+
+    //Setters
+    public void setState(State state)
+    {
+        this.state = state;
+    }
+}
