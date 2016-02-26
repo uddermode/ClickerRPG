b8218558c7270cbde18c01a8ae2512e23f6acb58
Edwin Avalos <eavalos0@gmail.com>
2016-02-25 20:32:49 -0700

    Forgot this one

---
+++ html/src/com/udderlevel/clickerrpg/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.udderlevel.clickerrpg.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.udderlevel.clickerrpg.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MyGdxGame();
+        }
+}
